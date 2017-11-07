b03376d04a1a925f7dd9eb550a9649cbea880ab7
Will Long <will.long043@gmail.com>
2017-11-06 19:22:19 -0500

    Initial commit.

---
+++ main.py
@@ -0,0 +1,46 @@
+class Transcript:
+    def __init__(self):
+        self.name = None
+        self.length = None
+        self.effectiveLength = None
+        self.tpm = None
+        self.numReads = None
+
+transcripts = {}
+
+def main():
+    # http://salmon.readthedocs.io/en/latest/file_formats.html#quantification-file
+    with open("quant.sf") as qsf:
+        # http://salmon.readthedocs.io/en/latest/file_formats.html#unique-and-ambiguous-count-file
+        with open("ambig_info.tsv") as amb:
+            qsfReader = csv.reader(qsf, dialect="excel-tab")
+            ambReader = csv.reader(amb, dialect="excel-tab")
+            next(reader, None) # skip header line
+            # load basic transcript info line by line
+            for line1, line2 in zip (qsfReader, ambReader):
+                t = Transcript()
+                t.name = line1[0]
+                t.length = line1[1]
+                t.effectiveLength = line1[2]
+                t.tpm = line1[3]
+                t.numReads = line1[4]
+                t.uniquelyMappedCount = line2[0]
+                t.ambiguouslyMappedCount = line2[1]
+                transcripts[t.name] = t
+
+    # http://salmon.readthedocs.io/en/latest/file_formats.html#equivalence-class-file
+    with open("eq_classes.txt") as eqc:
+        reader = csv.reader(eqc, dialect="excel-tab")
+        N = int(next(reader)[0])
+        M = int(next(reader)[0])
+        names = reader[:N] # take N transcript names
+        eq_classes = reader[:M] # take M equivalence classes
+
+            transcriptCount = eq_classes[0]
+            scripts = eq_classes[1:-2]
+            fragmentCount = eq_classes[-1]
+            
+
+
+if __name__ == "__main__": main()
+
